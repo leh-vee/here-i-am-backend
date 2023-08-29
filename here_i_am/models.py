83890ba708bbdbb6d8039c416e9e8453ba8ef9a9
Levi <levi@the-telling-detail.org>
2023-08-29 10:39:23 -0400

    create StreetSegment model and migration

--- here_i_am/models.py
+++ here_i_am/models.py
@@ -6,7 +6,19 @@ class Intersection(models.Model):
     geom = models.PointField()
 
     def __str__(self):
-      return self.description
+        return self.description
 
     class Meta:
-      db_table = 'intersection'
+        db_table = 'intersection'
+
+class StreetSegment(models.Model):
+    description = models.CharField()
+    from_intersection = models.ForeignKey(Intersection, on_delete=models.PROTECT, related_name='street_segments_from', null=True)
+    to_intersection = models.ForeignKey(Intersection, on_delete=models.PROTECT, related_name='street_segments_to', null=True)
+    geom = models.MultiLineStringField()
+
+    def __str__(self):
+        return self.description
+
+    class Meta:
+        db_table = 'street_segment'
