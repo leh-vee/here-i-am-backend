c5a6ab79760affd664d39b28cdbc4b90b59fb698
Levi <levi@the-telling-detail.org>
2023-09-11 18:15:35 -0400

    seed street nodes from street edge data set

--- here_i_am/models.py
+++ here_i_am/models.py
@@ -1,37 +1,33 @@
 from django.contrib.gis.db import models
 from django.db.models import Case, Value, When
 
-class IntersectionQuerySet(models.QuerySet):
-    def sefirot_ordered_by_intersection_id_index(self, intersection_ids):
+class StreetEdge(models.Model):
+    description = models.CharField()
+    from_street_node_id = models.IntegerField()
+    to_street_node_id = models.IntegerField()
+    geom = models.LineStringField()
+
+    def __str__(self):
+        return self.description
+
+    class Meta:
+        db_table = 'street_edge'
+
+class StreetNodeQuerySet(models.QuerySet):
+    def sefirot_ordered_by_street_node_id_index(self, street_node_ids):
         when_clauses = []
-        for index, intersection_id in enumerate(intersection_ids):
-            when_clauses.append(When(id=intersection_id, then=Value(index)))
+        for index, street_node_id in enumerate(street_node_ids):
+            when_clauses.append(When(id=street_node_id, then=Value(index)))
 
         order_statement = Case(*when_clauses)
 
-        return self.filter(id__in=intersection_ids).alias(order=order_statement).order_by("order")
+        return self.filter(id__in=street_node_ids).alias(order=order_statement).order_by("order")
 
-class Intersection(models.Model):
-    description = models.CharField()
-    classification = models.CharField()
+class StreetNode(models.Model):
     geom = models.PointField()
-
-    def __str__(self):
-        return self.description
+    n_street_edges = models.IntegerField()
 
     class Meta:
-        db_table = 'intersection'
+        db_table = 'street_node'
 
-    objects = IntersectionQuerySet.as_manager()
-
-class StreetSegment(models.Model):
-    description = models.CharField()
-    from_intersection = models.ForeignKey(Intersection, on_delete=models.PROTECT, related_name='street_segments_from', null=True)
-    to_intersection = models.ForeignKey(Intersection, on_delete=models.PROTECT, related_name='street_segments_to', null=True)
-    geom = models.MultiLineStringField()
-
-    def __str__(self):
-        return self.description
-
-    class Meta:
-        db_table = 'street_segment'
+    objects = StreetNodeQuerySet.as_manager()
--- here_i_am/views.py
+++ here_i_am/views.py
@@ -1,4 +1,4 @@
-from here_i_am.models import Intersection, StreetSegment
+from here_i_am.models import StreetNode, StreetEdge
 from django.contrib.gis.measure import D
 from django.core.serializers import serialize
 from django.http import JsonResponse
@@ -6,14 +6,14 @@ from django.shortcuts import render
 
 def intersections_index(request):
     geojson = serialize(
-        "geojson", Intersection.objects.all(), geometry_field="geom", 
+        "geojson", StreetNode.objects.all(), geometry_field="geom", 
         fields=["id", "description"]
     )
     return JsonResponse(geojson, safe=False)
 
 def sefirot(request):
     intersection_ids = [13465772, 14172266, 13463429, 13463848, 13464031, 13464314, 13464439, 13465037, 13464696, 13465233]
-    default_tree_intersections = Intersection.objects.sefirot_ordered_by_intersection_id_index(intersection_ids)
+    default_tree_intersections = StreetNode.objects.sefirot_ordered_by_intersection_id_index(intersection_ids)
     geojson = serialize(
         "geojson", default_tree_intersections, geometry_field="geom", 
         fields=["id", "description"]
@@ -22,14 +22,14 @@ def sefirot(request):
 
 def street_segments_index(request):
     geojson = serialize(
-        "geojson", StreetSegment.objects.all(), geometry_field="geom", 
+        "geojson", StreetEdge.objects.all(), geometry_field="geom", 
         fields=["id", "description"]
     )
     return JsonResponse(geojson, safe=False)
 
 def pathways(request, intersection_id, meters):
-    intersection = Intersection.objects.get(id=intersection_id)
-    ss = StreetSegment.objects.filter(
+    intersection = StreetNode.objects.get(id=intersection_id)
+    ss = StreetEdge.objects.filter(
         geom__distance_lte=(intersection.geom, D(m=meters))
     )
     geojson = serialize("geojson", ss, geometry_field="geom", fields=["id", "description"])
