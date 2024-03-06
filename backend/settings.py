0d03d276dbf005b3dfae26acebf795a7f82a7aef
Levi <levi@the-telling-detail.org>
2024-03-06 15:36:28 -0500

    get allowed hosts from env

--- backend/settings.py
+++ backend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["192.168.0.157"]
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,192.168.0.157,localhost").split(",")
 
 
 # Application definition
