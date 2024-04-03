abcf3d26d604d328b1d6727702cd4673a78d0fa5
Levi <levi@the-telling-detail.org>
2024-04-03 13:29:19 -0400

    use generic DB defaults

--- backend/settings.py
+++ backend/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.contrib.gis.db.backends.postgis',
         'NAME': os.getenv('DB_NAME', 'postgres'),
-        'USER': os.getenv('DB_USER', 'leonl'),
+        'USER': os.getenv('DB_USER', 'postgres'),
         'PASSWORD': os.getenv('DB_PASSWORD', ''),
         'HOST': os.getenv('DB_HOST', '127.0.0.1'),
         'PORT': os.getenv('DB_PORT', '5432'),
