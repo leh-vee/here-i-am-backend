118d5e87f73a1e943e3caab6673b10083faa5af7
Levi <levi@the-telling-detail.org>
2025-08-13 10:49:32 -0400

    text edit

--- here_i_am/views.py
+++ here_i_am/views.py
@@ -12,6 +12,19 @@ def street_nodes_index(request):
     )
     return HttpResponse(geojson, content_type="application/json")
 
+def random_street_nodes(request, n_nodes):
+    """
+    Returns GeoJSON for n random street nodes with 3 or more street edges.
+    """
+    # Limit the number of nodes to prevent excessive queries
+    max_nodes = min(n_nodes, 1000)
+    
+    nodes = StreetNode.objects.filter(n_street_edges__gte=3).order_by('?')[:max_nodes]
+    geojson = serialize(
+        "geojson", nodes, geometry_field="geom", fields=["n_street_edges"]
+    )
+    return HttpResponse(geojson, content_type="application/json")
+
 def tree_nodes(request):
     node_ids = [13465772, 14172266, 13463429, 13463848, 13464031, 13464314, 13464439, 13465037, 13464696, 13465233]
     nodes = StreetNode.objects.ordered_by_ids(node_ids)
@@ -46,18 +59,5 @@ def area_edges(request):
         "geojson", edges, geometry_field="geom", fields=[
             "description", "from_street_node_id", "to_street_node_id"
         ]
-    )
-    return HttpResponse(geojson, content_type="application/json")
-
-def random_street_nodes(request, n_nodes):
-    """
-    Returns GeoJSON for n random street nodes with 3 or more street edges.
-    """
-    # Limit the number of nodes to prevent excessive queries
-    max_nodes = min(n_nodes, 1000)
-    
-    nodes = StreetNode.objects.filter(n_street_edges__gte=3).order_by('?')[:max_nodes]
-    geojson = serialize(
-        "geojson", nodes, geometry_field="geom", fields=["n_street_edges"]
     )
     return HttpResponse(geojson, content_type="application/json")
